5fcbb3e3c409f0cc583f2c4d62ef4a911f5e7313
xkyy <710429130@qq.com>
2026-09-01 15:34:06 +0800

    fix: format IP share profile log

--- services/server/src/nearlink_host_server.cpp
+++ services/server/src/nearlink_host_server.cpp
@@ -688,7 +688,7 @@ bool NearlinkHostServer::impl::createServers()
     sptr<NearlinkIpShareServer> ipShareServer = new (std::nothrow) NearlinkIpShareServer();
     NL_CHECK_RETURN_RET(ipShareServer, false, "ipShareServer is nullptr");
     sleServers_[PROFILE_IPSHARE_SERVER] = ipShareServer->AsObject();
-    HILOGI("[IpShare][Host] IP share IPC server registered profile=%{public}d", PROFILE_IPSHARE_SERVER);
+    HILOGI("[IpShare][Host] IP share IPC server registered profile=%{public}s", PROFILE_IPSHARE_SERVER.c_str());
 
     if (!createPluginServer()) {
         return false;
